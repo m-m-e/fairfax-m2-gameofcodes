50288e85e29f33dfdec0a7baaee1b712258008bc
lararb <lararb91@gmail.com>
2019-04-03 13:36:52 +0200

    fix javascript fillName

--- _src/assets/js/fill.js
+++ _src/assets/js/fill.js
@@ -5,13 +5,27 @@
 const nameInput = document.querySelector('#name');
 const namePreview = document.querySelector('.preview--h2');
 
+const jobInput = document.querySelector('#job');
+const jobPreview = document.querySelector('.preview--h3');
+
 function fillName() {
-    event.preventDefault();
-    if (nameInput === '') {
-        namePreview.innerHTML = namePreview.innerHTML;
+
+    if (nameInput.value === '') {
+        namePreview.innerHTML = 'Nombre Apellido';
     } else {
         namePreview.innerHTML = nameInput.value;
     }
 }
 
+
 nameInput.addEventListener('keyup', fillName);
+
+function fillJob() {
+    if (jobInput.value === '') {
+        jobPreview.innerHTML = 'Front-end developer';
+    } else {
+        jobPreview.innerHTML = jobInput.value;
+    }
+}
+
+jobInput.addEventListener('keyup', fillJob);
