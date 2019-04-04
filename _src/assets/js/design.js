fb73294e0de30438209cf05137d8c063798551ed
arijdb <ariane.jurado@gmail.com>
2019-04-04 13:44:55 +0200

    add js palettes finish to review

---
+++ _src/assets/js/design.js
@@ -0,0 +1,48 @@
+
+
+// function changeColor() {
+//     previewCard.classList.add('preview-red');
+//     previewCard.classList.remove('preview-grey', 'preview-default');
+// }
+
+
+// radioRed.addEventListener('click', changeColor);
+
+// // grey
+
+// const radioGrey = document.querySelector('#grey_palette');
+
+
+
+
+// radioGrey.addEventListener('click', changeColor2);
+
+// //default
+
+// const radioDefault = document.querySelector('#blue_palette');
+
+
+// function changeColor3() {
+//     previewCard.classList.remove('preview-red', 'preview-grey');
+//     previewCard.classList.add('preview-default');
+// }
+
+'use strict';
+
+const radioBlue = document.querySelector('#blue_palette');
+const radioRed = document.querySelector('#red_palette');
+const radioGrey = document.querySelector('#grey_palette');
+
+const previewCard = document.querySelector('.preview__card');
+
+function changeColors(classtoadd) {
+    console.log(classtoadd)
+    previewCard.classList.remove('preview-grey','preview-red', 'preview-blue');
+    previewCard.classList.add(classtoadd);
+}
+
+
+radioBlue.addEventListener('click', function(){changeColors('preview-blue')});
+radioRed.addEventListener('click', function(){changeColors('preview-red')});
+radioGrey.addEventListener('click', function(){changeColors('preview-grey')});
+
