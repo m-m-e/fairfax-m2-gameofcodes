8351e425d595e3d32234c6a4d243075a2883dbb8
Angelica <angelica.cavero@gmail.com>
2019-04-10 11:27:26 +0200

    mail link fixed

--- _src/assets/js/fill.js
+++ _src/assets/js/fill.js
@@ -36,15 +36,10 @@ const inputEmail = document.querySelector('#email');
 const emailIcon = document.querySelector('.sm-link-mail');
 const valueEmail = inputEmail.value;
 
-// function addEmail() {
-//     email.classList.remove('hidden');
-//     emailIcon.href = `mailto:${value}`; 
-// };
-
 function showEmail() {
     if(inputEmail.value !== '') {
         email.classList.remove('hidden');
-        emailIcon.href = `mailto:${valueEmail}`;   
+        emailIcon.href = 'mailto:' + valueEmail;   
     }
     else {
         email.classList.add('hidden');
