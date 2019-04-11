55913ef2ae245f0a2a48da92c55289b07610ff43
lararb <lararb91@gmail.com>
2019-04-11 16:36:26 +0200

    fix collapsable js

--- _src/assets/js/collapsable.js
+++ _src/assets/js/collapsable.js
@@ -1,10 +1,31 @@
 
 const containerTitle = document.querySelectorAll('.js-container-title');
+const container = document.querySelectorAll('.js-container');
+
+
+function hidden() {
+    for (const panelHidden of container) {
+        panelHidden.classList.add('js-container-hidden');
+    }
+}
+
+function collapsable (e) {
+    //console.log(e.currentTarget.parentElement);
+    const parent = e.currentTarget.parentElement;
+    //parent.classList.toggle('js-container-hidden');
+    
+    if (parent.classList.contains('js-container-hidden') === true) {
+        hidden();
+        parent.classList.remove('js-container-hidden');
+    } else {
+        parent.classList.add('js-container-hidden');
+    }
+};
+
 for (const item of containerTitle) {
     item.addEventListener('click', collapsable);
 }
-function collapsable (e) {
-console.log(e.currentTarget.parentElement);
-e.currentTarget.parentElement.classList.toggle('js-container-hidden');
-}; 
+
+
+
 
