7bdc28d91b39eb4ce9793ed6b5a6a36e41cecec5
Megan Edwards <megmyfedwards@gmail.com>
2019-04-24 09:57:00 +0200

    Post to API with saved data

--- _src/assets/js/sendCard.js
+++ _src/assets/js/sendCard.js
@@ -1,33 +1,23 @@
 'use strict';
 
 const btnShare = document.querySelector('.share__btn--create');
-const nuestroObjeto = {
-    palette: 1,
-    name: 'Maria',
-    job: 'frontend',
-    phone: '66666666',
-    email: 'maria@gmail.com',
-    linkedin: 'mariagar',
-    github: 'mariagar',
-    photo: base64
-}
 
-function sendHandler() {
-    console.log('holi'); 
+//function sendHandler() {
+    //console.log('holi'); 
     fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
         method: 'POST',
         headers: {
             "content-Type": "application/json",
         },
-        body: JSON.stringify(nuestroObjeto),
+        body: JSON.stringify(cardData),
     })
     .then(response => response.json())
     .then(data => {
         const urlGenerated = data.cardURL;
         console.log(urlGenerated);
     })
-}
+//}
 
-//sendCard();
+//sendHandler();
 
-btnShare.addEventListener('click', sendHandler);
+// btnShare.addEventListener('click', sendHandler);
