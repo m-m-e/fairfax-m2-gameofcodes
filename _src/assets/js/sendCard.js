463bff64cfb9a12bc75c29ef5c7add6844da3508
lararb <lararb91@gmail.com>
2019-04-24 09:09:37 +0200

    change form action url

--- _src/assets/js/sendCard.js
+++ _src/assets/js/sendCard.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const btnShare = document.querySelector('.share__btn--create');
 const nuestroObjeto = {
     palette: 1,
     name: 'Maria',
@@ -11,16 +12,23 @@ const nuestroObjeto = {
     photo: base64
 }
 
+function sendHandler() {
+    console.log('holi'); 
 
-fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-    method: 'POST',
-    headers: {
-        "content-Type": "application/json",
-    },
-    body: JSON.stringify(nuestroObjeto),
-})
-.then(response => response.json())
-.then(data => {
-    const urlGenerated = data.cardURL;
-    console.log(urlGenerated);
-})
+    fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+        method: 'POST',
+        headers: {
+            "content-Type": "application/json",
+        },
+        body: JSON.stringify(nuestroObjeto),
+    })
+    .then(response => response.json())
+    .then(data => {
+        const urlGenerated = data.cardURL;
+        console.log(urlGenerated);
+    })
+}
+
+//sendCard();
+
+btnShare.addEventListener('click', sendHandler);
