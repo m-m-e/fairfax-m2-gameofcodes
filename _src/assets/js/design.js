92119379f9811fe306f252873dbdea67503e51b9
arijdb <ariane.jurado@gmail.com>
2019-04-29 12:02:52 +0200

    some indent

--- _src/assets/js/design.js
+++ _src/assets/js/design.js
@@ -7,28 +7,28 @@ const radioGrey = document.querySelector('#grey_palette');
 const previewCard = document.querySelector('.preview__card');
 
 function changeColors(classtoadd) {
-    previewCard.classList.remove('preview-grey','preview-red', 'preview-blue');
-    previewCard.classList.add(classtoadd);
+  previewCard.classList.remove('preview-grey', 'preview-red', 'preview-blue');
+  previewCard.classList.add(classtoadd);
 }
 
-function choosePalette(value){
-    if (value === 1) {
-        changeColors('preview-blue');
-        radioBlue.checked = 'checked';
-    } else if (value === 2) {
-        changeColors('preview-red');
-        radioRed.checked = 'checked';
-    } else {
-        changeColors('preview-grey');
-        radioGrey.checked = 'checked';
-    }
+function choosePalette(value) {
+  if (value === 1) {
+    changeColors('preview-blue');
+    radioBlue.checked = 'checked';
+  } else if (value === 2) {
+    changeColors('preview-red');
+    radioRed.checked = 'checked';
+  } else {
+    changeColors('preview-grey');
+    radioGrey.checked = 'checked';
+  }
 }
 
 const getPalette = event => {
-    const name = event.currentTarget.name;
-    const value = parseInt(event.currentTarget.value);
-    choosePalette(value);
-    saveData(name, value);
+  const name = event.currentTarget.name;
+  const value = parseInt(event.currentTarget.value);
+  choosePalette(value);
+  saveData(name, value);
 };
 
 radioBlue.addEventListener('click', getPalette);
--- _src/assets/js/fill.js
+++ _src/assets/js/fill.js
@@ -30,52 +30,51 @@ const imageDefault = document.querySelector('.preview__card--image');
 const iconsDefault = document.querySelectorAll('.sm');
 const imgPreviewDefault = document.querySelector('.form__photo-preview');
 
-
 const card = {
-    name: 'Nombre Apellido',
-    job: 'Front-end developer',
-    email: '',
-    phone: '',
-    linkedin: '',
-    github: '',
-    photo: base64,
-    palette: 1
+  name: 'Nombre Apellido',
+  job: 'Front-end developer',
+  email: '',
+  phone: '',
+  linkedin: '',
+  github: '',
+  photo: base64,
+  palette: 1
 };
 
 const savedData = JSON.parse(localStorage.getItem('cardSaved'));
 
 const cardData = {
-    ...card,
-    ...savedData
+  ...card,
+  ...savedData
 };
 
-const setCardData = (data) => {
-    if (data.name === 'Nombre Apellido') {
-        namePreview.innerHTML = data.name;
-    } else {
-        nameInput.value = data.name;
-        namePreview.innerHTML = data.name;
-    }
-    fillName();
-    
-    if (data.job === 'Front-end developer') {
-        jobPreview.innerHTML = data.job;        
-    } else {
-        jobInput.value = data.job;
-        jobPreview.innerHTML = data.job;
-    }
-    fillJob();
-    inputEmail.value = data.email;
-    showEmail();
-    inputPhone.value = data.phone;
-    showPhone();
-    inputLinkedin.value = data.linkedin;
-    showLinkedin();
-    inputGithub.value = data.github;
-    showGithub();
-    choosePalette(cardData.palette);
-    imageDefault.style = `background-image: url(${cardData.photo})`;
-    imgPreviewDefault.style = `background-image: url(${cardData.photo})`;
+const setCardData = data => {
+  if (data.name === 'Nombre Apellido') {
+    namePreview.innerHTML = data.name;
+  } else {
+    nameInput.value = data.name;
+    namePreview.innerHTML = data.name;
+  }
+  fillName();
+
+  if (data.job === 'Front-end developer') {
+    jobPreview.innerHTML = data.job;
+  } else {
+    jobInput.value = data.job;
+    jobPreview.innerHTML = data.job;
+  }
+  fillJob();
+  inputEmail.value = data.email;
+  showEmail();
+  inputPhone.value = data.phone;
+  showPhone();
+  inputLinkedin.value = data.linkedin;
+  showLinkedin();
+  inputGithub.value = data.github;
+  showGithub();
+  choosePalette(cardData.palette);
+  imageDefault.style = `background-image: url(${cardData.photo})`;
+  imgPreviewDefault.style = `background-image: url(${cardData.photo})`;
 };
 
 setCardData(cardData);
@@ -83,24 +82,23 @@ setCardData(cardData);
 //nombre y puesto
 
 function fillName() {
-    if (nameInput.value === '') {
-        namePreview.innerHTML = card.name;
-    } else {
-        namePreview.innerHTML = nameInput.value;
-    }
-    saveData(nameInput.name, nameInput.value);
-
+  if (nameInput.value === '') {
+    namePreview.innerHTML = card.name;
+  } else {
+    namePreview.innerHTML = nameInput.value;
+  }
+  saveData(nameInput.name, nameInput.value);
 }
 
 nameInput.addEventListener('keyup', fillName);
 
 function fillJob() {
-    if (jobInput.value === '') {
-        jobPreview.innerHTML = card.job;
-    } else {
-        jobPreview.innerHTML = jobInput.value;
-    }
-    saveData(jobInput.name, jobInput.value);
+  if (jobInput.value === '') {
+    jobPreview.innerHTML = card.job;
+  } else {
+    jobPreview.innerHTML = jobInput.value;
+  }
+  saveData(jobInput.name, jobInput.value);
 }
 
 jobInput.addEventListener('keyup', fillJob);
@@ -109,101 +107,94 @@ jobInput.addEventListener('keyup', fillJob);
 // MAIL
 
 function showEmail() {
-    cardData.email = inputEmail.value;
-    if(cardData.email !== '') {
-        email.classList.remove('hidden');
-        emailIcon.href = 'mailto:' + valueEmail;
-    }
-    else {
-        email.classList.add('hidden');
-    }   
-    saveData(inputEmail.name, inputEmail.value);
-};
+  cardData.email = inputEmail.value;
+  if (cardData.email !== '') {
+    email.classList.remove('hidden');
+    emailIcon.href = 'mailto:' + valueEmail;
+  } else {
+    email.classList.add('hidden');
+  }
+  saveData(inputEmail.name, inputEmail.value);
+}
 
 inputEmail.addEventListener('keyup', showEmail);
 
 //PHONE
 
 function showPhone() {
-    cardData.phone = inputPhone.value;
-    if(cardData.phone !== '') {
-        phone.classList.remove('hidden');
-        phoneIcon.href = `tel:${valuePhone}`;
-    }
-    else {
-        phone.classList.add('hidden');
-    }    
-    saveData(inputPhone.name, inputPhone.value);
-};
+  cardData.phone = inputPhone.value;
+  if (cardData.phone !== '') {
+    phone.classList.remove('hidden');
+    phoneIcon.href = `tel:${valuePhone}`;
+  } else {
+    phone.classList.add('hidden');
+  }
+  saveData(inputPhone.name, inputPhone.value);
+}
 
 inputPhone.addEventListener('keyup', showPhone);
 
 //LINKEDIN
 
 function showLinkedin() {
-    cardData.linkedin = inputLinkedin.value;
-    if(cardData.linkedin !== '') {
-        linkedin.classList.remove('hidden');
-        linkedinIcon.href = 'https://www.linkedin.com/in/' + cardData.linkedin;
-    }
-    else {
-        linkedin.classList.add('hidden');
-    }  
-    saveData(inputLinkedin.name, inputLinkedin.value);    
-};
+  cardData.linkedin = inputLinkedin.value;
+  if (cardData.linkedin !== '') {
+    linkedin.classList.remove('hidden');
+    linkedinIcon.href = 'https://www.linkedin.com/in/' + cardData.linkedin;
+  } else {
+    linkedin.classList.add('hidden');
+  }
+  saveData(inputLinkedin.name, inputLinkedin.value);
+}
 
 inputLinkedin.addEventListener('keyup', showLinkedin);
 
 //GITHUB
 
 function showGithub() {
-    cardData.github = inputGithub.value;
-    if(cardData.github !== '') {
-        github.classList.remove('hidden');
-        githubIcon.href = 'https://github.com/' + cardData.github;
-    }
-    else {
-        github.classList.add('hidden');
-    }  
-    saveData(inputGithub.name, inputGithub.value);
-};
+  cardData.github = inputGithub.value;
+  if (cardData.github !== '') {
+    github.classList.remove('hidden');
+    githubIcon.href = 'https://github.com/' + cardData.github;
+  } else {
+    github.classList.add('hidden');
+  }
+  saveData(inputGithub.name, inputGithub.value);
+}
 
 inputGithub.addEventListener('keyup', showGithub);
 
 //RESET
 
-function hideIcons () {
-    for (const icon of iconsDefault) {
+function hideIcons() {
+  for (const icon of iconsDefault) {
     icon.classList.add('hidden');
-    }
+  }
 }
 
-function backImage () {
-    imageDefault.style.backgroundImage = `url(${card.photo})`;
+function backImage() {
+  imageDefault.style.backgroundImage = `url(${card.photo})`;
 }
 
-function resetCard (event) {
-    for (const item of field) {
-        item.value = '';
-        saveData(item.name, item.value);
-    }
-    choosePalette(card.palette);
-    cardData.palette = '1';
-    hideIcons();  
-    fillName();
-    fillJob(); 
-    changeColors('preview-blue');
-    backImage();
-    profilePreview.style.backgroundImage = '';
+function resetCard(event) {
+  for (const item of field) {
+    item.value = '';
+    saveData(item.name, item.value);
+  }
+  choosePalette(card.palette);
+  cardData.palette = '1';
+  hideIcons();
+  fillName();
+  fillJob();
+  changeColors('preview-blue');
+  backImage();
+  profilePreview.style.backgroundImage = '';
 }
 
 reset.addEventListener('click', resetCard);
 
-
 function saveData(key, val) {
-    cardData[key] = val;
-    localStorage.setItem('cardSaved', JSON.stringify(cardData));
-    return cardData;
+  cardData[key] = val;
+  localStorage.setItem('cardSaved', JSON.stringify(cardData));
+  return cardData;
 }
-
-
